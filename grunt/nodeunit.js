639a73cfa99cd2319434f93e21e9a8913ffd7bce
Ying <xueying19981206@gmail.com>
2020-04-19 16:37:17 +0200

    Add Yang module: weather report by Yang yang

--- grunt/nodeunit.js
+++ grunt/nodeunit.js
@@ -1,4 +1,4 @@
 /*global module */
 module.exports = {
-    main: ['MMM-CurrencyRate/test.js', 'MMM-Demotivational/test.js']
+    main: ['MMM-CurrencyRate/test.js', 'MMM-Demotivational/test.js', 'MMM-Yang/test.js']
 };
